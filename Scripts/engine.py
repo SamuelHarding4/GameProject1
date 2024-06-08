5a299dd7f5c309b13d843a57742bf11f897e902a
zlac212 <zlac212@live.rhul.ac.uk>
2024-06-08 12:55:07 +0100

    added basic pygame window

--- Scripts/engine.py
+++ Scripts/engine.py
@@ -2,5 +2,12 @@ import pygame
 from vector import *
 
 class Window:
-    def __init__(self):
-        print("hello")
+    def __init__(self, width,height, fullscreen, title):
+        self.width = width
+        self.height = height
+        self.fullscreen = fullscreen
+        self.title = title
+
+        # makes the pygame window
+        self.window = pygame.display.set_mode((width,height), fullscreen)
+
