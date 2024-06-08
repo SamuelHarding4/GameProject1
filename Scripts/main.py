62b6733e45635f38689fbc70b3a643880a628a60
zlac212 <zlac212@live.rhul.ac.uk>
2024-06-08 12:55:34 +0100

    main runs a pygame window

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,22 @@
 from engine import *
 
-window = Window()
+
+pygame.init()
+
+#base window details
+width = 1000
+height = 1000
+fullscreen = 0 # 0  for defined width and height, pygame.FULLSCREEN for fullscreen
+title = "GameProject"
+
+#window handling
+window = Window(width, height, fullscreen, title)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
